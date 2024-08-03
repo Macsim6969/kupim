8cb416ee2e5452433d5f3fc3b1ad4383355712db
MacsimB <macs.belousov666@gmail.com>
2024-08-03 19:43:30 +0300

    updated

--- src/app/modules/welcome/welcome.component.ts
+++ src/app/modules/welcome/welcome.component.ts
@@ -3,6 +3,7 @@ import { SidebarServcie } from '../../shared/services/sidebar.service';
 import { Subject, takeUntil } from 'rxjs';
 import { TranslateService } from '@ngx-translate/core';
 import { Welcome } from './shared/interfaces/welcome.interface';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-welcome',
@@ -13,18 +14,26 @@ export class WelcomeComponent implements OnInit, OnDestroy {
   private destroy$: Subject<void> = new Subject<void>();
 
   public welcomeDataPage: Welcome;
+  public state: string
   constructor(
     private sidebarService: SidebarServcie,
-    private translate: TranslateService
+    private translate: TranslateService,
+    private route: ActivatedRoute
   ) { }
   ngOnInit(): void {
     this.streamDashboardDataFromJson();
+    this.route.queryParams.subscribe((data) => this.state = this.capitalizeFirstLetter(data['state']))
   }
 
   private streamDashboardDataFromJson() {
     this.translate.stream('dashboard').pipe(takeUntil(this.destroy$)).subscribe((data: Welcome) => {
       this.welcomeDataPage = data;
     })
+  }
+
+  private capitalizeFirstLetter(value: string): string {
+    if (!value) return value;
+    return value.charAt(0).toUpperCase() + value.slice(1);
   }
 
   public openSidebar() {
