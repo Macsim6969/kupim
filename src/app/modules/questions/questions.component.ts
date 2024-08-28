f5ea2a3e286f52994ebea27c93c9b57a99638e65
MacsimB <macs.belousov666@gmail.com>
2024-08-28 20:31:30 +0300

    updated

--- src/app/modules/questions/questions.component.ts
+++ src/app/modules/questions/questions.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, QueryList, ViewChildren } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { Subject, takeUntil } from 'rxjs';
 import { Questions } from './shared/interfaces/questions.interface';
@@ -13,6 +13,8 @@ export class QuestionsComponent implements OnInit, OnDestroy {
   public qustionsData: Questions;
   public isOpenTab: boolean[] = [];
   private isOpen: boolean;
+
+  @ViewChildren('doppContent') public contents: QueryList<ElementRef>;
   constructor(
     private translate: TranslateService
   ) { }
@@ -28,12 +30,19 @@ export class QuestionsComponent implements OnInit, OnDestroy {
       })
   }
 
-  public openTab(id: number) {
+  openTab(id: number) {
     this.isOpen = !this.isOpen;
     this.isOpenTab = [];
     this.isOpenTab[id] = this.isOpen;
   }
 
+  getContentMaxHeight(index: number): string {
+    if (this.contents && this.contents.length > 0) {
+      const contentElement = this.contents.toArray()[index]?.nativeElement;
+      return contentElement ? contentElement.scrollHeight + 'px' : '0';
+    }
+    return '0';
+  }
   ngOnDestroy(): void {
     this.destroy$.next();
     this.destroy$.complete();
