ee2c2607fcd41634fd56c2d66e6b3b32ffe44d91
MacsimB <macs.belousov666@gmail.com>
2024-11-29 12:08:27 +0200

    updated main page

--- src/app/shared/services/information.service.ts
+++ src/app/shared/services/information.service.ts
@@ -1,43 +1,42 @@
 import { Injectable } from '@angular/core';
 
 export interface DataInforamtion {
-  address: string
-  dayWork: { title: string }[]
-  telNumber: string
-  email: string
+  address: string;
+  dayWork: { title: string }[];
+  telNumber: string;
+  email: string;
 }
 
 @Injectable()
-
 export class InformationService {
   private dataInform: DataInforamtion = {
-    address: "12805 SW 6th St, Miami, FL 33184, United States",
+    address: '12805 SW 6th St, Miami, FL 33184, United States',
     dayWork: [
       {
-        title: "Monday - 09:00 - 19:00"
+        title: 'Monday - 09:00 AM - 07:00 PM',
       },
       {
-        title: "Tuesday - 09:00 - 19:00"
+        title: 'Tuesday - 09:00 AM - 07:00 PM',
       },
       {
-        title: "Wednesday - 09:00 - 19:00"
+        title: 'Wednesday - 09:00 AM - 07:00 PM',
       },
       {
-        title: "Thursday - 09:00 - 19:00"
+        title: 'Thursday - 09:00 AM - 07:00 PM',
       },
       {
-        title: "Friday - 09:00 - 19:00"
+        title: 'Friday - 09:00 AM - 07:00 PM',
       },
       {
-        title: "Saturday - 09:00 - 19:00"
+        title: 'Saturday - 09:00 AM - 07:00 PM',
       },
       {
-        title: "Sunday - 09:00 - 19:00"
-      }
+        title: 'Sunday - 09:00 AM - 07:00 PM',
+      },
     ],
-    telNumber: "+1 727 509 14 49",
-    email: "Email: namegmail.com"
-  }
+    telNumber: '+1 727 509 14 49',
+    email: 'Email: namegmail.com',
+  };
 
   get _dataInformation(): DataInforamtion {
     return this.dataInform;
